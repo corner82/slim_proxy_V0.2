9df1be6d415f90f34ab79bb5312204a59f4a05c7
corner82 <311corner82@gmail.com>
2015-05-22 11:46:53 +0300

    Merge branch 'master' of https://github.com/corner82/slim_proxy_V0.2
    Conflicts:
    	vendor/hmac/Hmac.php

--- vendor/hmac/Hmac.php
+++ vendor/hmac/Hmac.php
@@ -1,130 +1,3 @@
-<<<<<<< HEAD
-<?php
-
-/**
- * Rest Api Proxy Library
- *
- * @author Zeynel Dağlı
- * @version 0.2
- */
-namespace vendor\hmac;
-
-class Hmac {
-    
-    protected $hash;
-    
-    protected $publicKey;
-    
-    protected $privateKey;
-    
-    protected $requestParams = array();
-    
-    protected $nonce = null;
-    
-    protected $timeStamp = null;
-
-    public function __construct() {
-        
-    }
-    
-    public function setHash($hash = null) {
-        $this->hash = $hash;
-    }
-    
-    public function getHash() {
-        return $this->hash;
-    }
-    
-    private function setNonce($nonce = null) {
-        if($nonce == null) {
-            $this->nonce = md5(time().rand());
-        } else {
-            $this->nonce = $nonce;
-        }        
-        //print_r('!!!!'.$this->nonce.'!!!!');
-    }
-    
-    private function getNonce() {
-        //if($this->nonce==null) $this->setNonce();
-        //print_r('// get nonce()--'.$this->nonce.'//');
-        return $this->nonce;
-    }
-    
-     /**
-     * set timestamp 
-     * framework
-     * @param string $timeStamp
-     * @author Okan Cıran
-     * @version 0.0.1
-     */
-      public function setTimeStamp($timeStamp = null) {
-        if($timeStamp == null) {
-            $this->timeStamp = time();
-         } else {
-            $this->timeStamp = $timeStamp;
-        }   
-    }
-    
-     /**
-     * get timestamp 
-     * framework
-     * 
-     * @author Okan Cıran
-     * @version 0.0.1
-     */
-    public function getTimeStamp() {
-        return $this->timeStamp;
-    }   
-    
-     /**
-     * difference timestamp 
-     * framework
-     * @param string $timeStamp
-     * @author Okan Cıran
-     * @version 0.0.1
-     */
-      public function differenceTimeStamp() {
-        if(getTimeStamp() != null) {
-            return time() - getTimeStamp();
-         } else {
-            return null;
-        } 
-         print_r('// differenceTimeStamp()--'.$this->differenceTimeStamp().'//');
-    } 
-       
-    public function makeHmac() {
-        //HMAC(HMAC(MESSAGE, user_password), application_key)
-        $this->setNonce();
-        $this->hash = hash_hmac('sha256', hash_hmac('sha256', json_encode($this->requestParams),  $this->getNonce()), $this->privateKey);
-        //$this->hash = hash_hmac('sha256', json_encode($this->requestParams), $this->privateKey);
-    }
-    
-    public function setPublicKey($publicKey = null) {
-        $this->publicKey = $publicKey;
-    } 
-    
-    public function getPublicKey() {
-        return $this->publicKey;
-    }
-    
-    public function setPrivateKey($privateKey = null) {
-        $this->privateKey = $privateKey;
-    }
-    
-    public function getPrivateKey() {
-        return $this->privateKey;
-    }
-    
-    public function setRequestParams($requestParams = null) {
-        $this->requestParams = $requestParams;
-    }
-    
-    public function getRequestParams() {
-        return $this->requestParams;
-    }
-}
-
-=======
 <?php
 
 /**
@@ -183,7 +56,7 @@ class Hmac {
      * @author Okan Cıran
      * @version 0.0.1
      */
-      public function setTimeStamp($timeStamp = null) {
+      protected function setTimeStamp($timeStamp = null) {
         if($timeStamp == null) {
             $this->timeStamp = time();
          } else {
@@ -198,7 +71,7 @@ class Hmac {
      * @author Okan Cıran
      * @version 0.0.1
      */
-    public function getTimeStamp() {
+    protected function getTimeStamp() {
         return $this->timeStamp;
     }   
     
@@ -209,7 +82,7 @@ class Hmac {
      * @author Okan Cıran
      * @version 0.0.1
      */
-      public function differenceTimeStamp() {
+      protected function differenceTimeStamp() {
         if(getTimeStamp() != null) {
             return time() - getTimeStamp();
          } else {
@@ -249,5 +122,3 @@ class Hmac {
         return $this->requestParams;
     }
 }
-
->>>>>>> 8fc02dc6bf8a3c22d69223175f88beb4e04d048d
